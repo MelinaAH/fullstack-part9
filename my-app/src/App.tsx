d471b08adc27cdf9f2dbc14ca47c04baab92a298
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-04-27 16:17:37 +0300

    9.15 done

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,18 +3,79 @@ import './App.css';
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    kind: string;
+  }
+
+  interface CoursePartDescription extends CoursePartBase {
+    description: string;
+  }
+
+  interface CoursePartRequirements extends CoursePartBase {
+    requirements: string[];
+  }
+
+  interface CoursePartBasic extends CoursePartDescription {
+    kind: "basic"
+  }
+
+  interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group"
+  }
+
+  interface CoursePartBackground extends CoursePartDescription {
+    backgroundMaterial: string;
+    kind: "background"
+  }
+
+  interface CoursePartSpecial extends CoursePartBase, CoursePartDescription, CoursePartRequirements {
+    kind: "special";
+  }
+
+  type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial;
+
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic",
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group",
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic",
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background",
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
   ];
 
@@ -27,19 +88,51 @@ const App = () => {
   };
 
   interface ContentProps {
-    courseParts: ContentPart[];
+    courseParts: CoursePart[];
   }
 
-  interface ContentPart {
-    name: string;
-    exerciseCount: number;
-  }
+  const Part = ({ part }: { part: CoursePart }) => {
+    switch (part.kind) {
+      case 'basic':
+        return (
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p style={{ fontStyle: 'italic' }}>{part.description}</p>
+          </div>
+        )
+      case 'group':
+        return (
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p>project exercises {part.groupProjectCount}</p>
+          </div>
+        )
+      case 'background':
+        return (
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p style={{ fontStyle: 'italic' }}>{part.description}</p>
+            <p>submit to {part.backgroundMaterial}</p>
+          </div>
+        )
+      case 'special':
+        return (
+          <div>
+            <h3>{part.name} {part.exerciseCount}</h3>
+            <p style={{ fontStyle: 'italic' }}>{part.description}</p>
+            <p>required skills: {part.requirements.join(", ")}</p>
+          </div>
+        )
+      default:
+        return null;
+    }
+  };
 
   const Content = (props: ContentProps) => {
     return (
       <>
         {props.courseParts.map((part) => (
-          <p key={part.name}>{part.name} {part.exerciseCount}</p>
+          <Part key={part.name} part={part} />
         ))}
       </>
     )
@@ -47,10 +140,13 @@ const App = () => {
 
   const Total = (props: ContentProps) => {
     return (
-      <p>
-        Number of exercises{" "}
-        {props.courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
-      </p>
+      <div>
+        <p>
+          Number of exercises{" "}
+          {props.courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+        </p>
+      </div>
+
     )
   };
 
